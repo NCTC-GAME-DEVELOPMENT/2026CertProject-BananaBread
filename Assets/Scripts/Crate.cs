bc54e056c376c3863c907e7865f53ef52dec1dd6
CatBio908 <powellt416222@student.nctc.edu>
2026-03-24 17:19:19 -0500

    End of Day 3

--- Assets/Scripts/Crate.cs
+++ Assets/Scripts/Crate.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Crate : MonoBehaviour
 {
-    public int[] currentPos;
-    public float moveAmount = 3.0f;
+    public Grid_testing gt;
+    public int PosX;
+    public int PosY;
     Vector3 myPosition;
+    float moveAmount = 3.0f;
 
     //PlayerController senses a crate to push in front of it.
     //If success, pushes the crate in its facing direction, moving x or y based on the current push direction.
 
     private void Start()
     {
+        gt.grid.SetValue(PosX, PosY, (2));
         myPosition = gameObject.transform.position;
     }
     public void MoveCrate(string direction)
@@ -18,19 +21,35 @@ public class Crate : MonoBehaviour
         Debug.Log("Push Detected");
         if (direction == "North")
         {
+            gt.grid.SetValue(PosX, PosY, (0));
+            PosY += 1;
+            gt.grid.SetValue(PosX, PosY, (2));
             gameObject.transform.position = new Vector3(myPosition.x, myPosition.y, (myPosition.z + moveAmount));
+            myPosition = gameObject.transform.position;
         }
         else if (direction == "South")
         {
+            gt.grid.SetValue(PosX, PosY, (0));
+            PosY -= 1;
+            gt.grid.SetValue(PosX, PosY, (2));
             gameObject.transform.position = new Vector3(myPosition.x, myPosition.y, (myPosition.z - moveAmount));
+            myPosition = gameObject.transform.position;
         }
         else if (direction == "East")
         {
+            gt.grid.SetValue(PosX, PosY, (0));
+            PosX += 1;
+            gt.grid.SetValue(PosX, PosY, (2));
             gameObject.transform.position = new Vector3((myPosition.x + moveAmount), myPosition.y, myPosition.z);
+            myPosition = gameObject.transform.position;
         }
         else
         {
+            gt.grid.SetValue(PosX, PosY, (0));
+            PosX -= 1;
+            gt.grid.SetValue(PosX, PosY, (2));
             gameObject.transform.position = new Vector3((myPosition.x - moveAmount), myPosition.y, myPosition.z);
+            myPosition = gameObject.transform.position;
         }
     }
 }
