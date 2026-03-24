c1a6871242a225d88a44298f10abd9d5f694d9d8
Eletriky <kylecowsert15@gmail.com>
2026-03-24 15:33:36 -0500

    Exit Door
    Added basic Exit Door function.

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+
+    // Bool to check if crate has been sent.
+    public bool CrateSent = false;
+    // Bool check for first player going through.
+    public bool PlayerLeft = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Create variables for player and crate.
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+        PlaceholderWinCondition winCrate = collision.gameObject.GetComponent<PlaceholderWinCondition>();
+
+        // If it's the crate, declare it being sent as true and delete the crate.
+        if(winCrate)
+        {
+            CrateSent = true;
+            Destroy(collision.gameObject);
+        }
+
+        if (player && !PlayerLeft)
+        {
+            Destroy(collision.gameObject);
+            PlayerLeft = true;
+        }
+
+        // If the player enters after the create is sent, win.
+        if (player && PlayerLeft && CrateSent)
+        {
+            PlayerWins();
+        }
+    }
+
+    // Made a separate function for simpler modifying and use.
+    public void PlayerWins()
+    {
+        Debug.Log("You win!");
+    }
+}
