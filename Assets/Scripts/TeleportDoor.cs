3f2a2448ec8db4b240d67d2e4adc1ced90f086d6
Eletriky <kylecowsert15@gmail.com>
2026-03-26 16:00:24 -0500

    Door updates
    Removed placeholder script, altered ExitDoor to look for QueryCrate, and refined TeleportDoor to teleport to center of cells.
    
    Also, added 4 lines of code in quoted area to TeleportDoor that would allow objects to teleport to their destination in grid.

--- Assets/Scripts/TeleportDoor.cs
+++ Assets/Scripts/TeleportDoor.cs
@@ -7,6 +7,7 @@ public class TeleportDoor : MonoBehaviour
 
     // Choose grid location for exit.
     public int gridX, gridY;
+    public bool DestinationNorthWall = false;
 
     //Game Manager variable.
     private GameObject manager;
@@ -25,12 +26,21 @@ public class TeleportDoor : MonoBehaviour
         // If player controller, teleport to empty.
         if (player)
         {
+
+            // Get the grid.
+            Grid_testing grid = manager.GetComponent<Grid_testing>();
             // Find destination.
-            Vector3 destination = manager.GetComponent<Grid_testing>().grid.GetWorldPosition(gridX, gridY);
+            Vector3 destination = grid.grid.GetWorldPosition(gridX, gridY);
             // Get destination cell's value.
-            int destinationSpace = manager.GetComponent<Grid_testing>().grid.GetValue(gridX, gridY);
+            int destinationSpace = grid.grid.GetValue(gridX, gridY);
             // Correct the Y so that it isn't embedded in the floor.
             destination.y = 1f;
+
+            // Add half the cell size to center it on the grid cell.
+            destination.x = destination.x + (grid.cellSize/2f);
+            if (DestinationNorthWall) destination.z = destination.z - (grid.cellSize / 2f);
+            else destination.z = destination.z + (grid.cellSize / 2f);
+
             // If the grid is a 0 value...
             if (destinationSpace == 0)
             {
@@ -40,3 +50,22 @@ public class TeleportDoor : MonoBehaviour
         }
     }
 }
+/*
+ * Pseudo code idea: how to handle crates going through the teleport door.
+ * 
+ * First: the collission box only checks if it enters collision, not teleports. It has a bool for "inFrontOfDoor".
+ * 
+ * Second: only teleports if pushed into the door.
+ * 
+ * Third: If a player enters, and it's blocked by CRATE, attempt to push that crate outward.
+ * May need to add the north/south/east/west enum here to tell it which way to attempt to shove the crate in the destination.
+ * If it works, teleport player after the push.
+ * 
+ * 
+ * Also, these lines:         
+ * Vector3 location = gameObject.transform.position = gt.grid.GetWorldPosition(PosX, PosY);
+ * location.x = location.x + (grid.cellSize/2f);
+ * location.z = location.z + (grid.cellSize/2f);
+ * gameObject.transform.position = location;
+ * I feel this and similar cellsize adjustments should be used to teleport to set location in the grid.
+ */
