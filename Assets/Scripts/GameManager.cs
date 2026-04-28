5c9a12022b30a17226d0d896812c2671d1f26b90
Eletriky <kylecowsert15@gmail.com>
2026-04-28 15:37:00 -0500

    Camera
    Unfinished camera code in the GameManager code.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using TMPro;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 using Debug = UnityEngine.Debug;
 
 public class GameManager : Info
@@ -20,8 +19,44 @@ public class GameManager : Info
     public TextMeshProUGUI LevelTime;
     public GameObject StopwatchManager;
 
+    // For the camera.
+    private Camera mainCam;
+    // Grabbed only for the camera math.
+    private Grid_testing gt;
+
     private void Start()
     {
+        /*
+         * 
+         * Didn't get it working yet.
+         * 
+         * 
+        // Get the grid.
+        gt = GameObject.Find("GameManager").GetComponent<Grid_testing>();
+
+        // Get the camera.
+        mainCam = Camera.main;
+
+        // The math is: with about a 2:1 level design, 7 to the camera's Y per 2:1 makes it viewable.
+        // So, if I grab X of the grid and multiply it by 7, we get a useable Y height for the camera.
+        // May as well set the Z and rotation by default as well.
+        // -61.4 Z, and 51.9 X rotation. May as well see how those look rounded.
+
+        // Let's try this.
+        // Get camera minus game manager location.
+        Vector3 direction = gt.transform.position - mainCam.transform.position;
+        // Create a rotation towards that direction.
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        // Extract only the X rotation.
+        Vector3 rotation = transform.eulerAngles;
+        rotation.x = lookRotation.eulerAngles.x * 1.5f; // Times 1.5 because that works better.
+        mainCam.transform.eulerAngles = rotation;
+
+
+
+        mainCam.transform.position = new Vector3(0f, (gt.height * 7f), (gt.height * -3.5f));
+        */
+
         if (GameObject.Find("StopwatchManager") == false && GameObject.Find("StopwatchManager(Clone)") == false)
         {
             Debug.Log("Stopwatch not detected. Making one now");
