7c1fa3a909079e36c9df09bb7a0db0ef0692d149
CatBio908 <powellt416222@student.nctc.edu>
2026-05-05 15:17:06 -0500

    Menu Changes + Logo

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -4,20 +4,28 @@ using UnityEngine.UI;
 
 public class mainMenu : MonoBehaviour
 {
-    
-
-   
-
     public Button StartButton;
     public Button SelectLevelButton;
     public Button QuitButton;
     public Button CreditsButton;
-    public string Credits;
+    public Button BackButtonCredits;
+    public Button BackButtonLevel;
     public string FirstLevel;
-    public string LevelSelect;
+    public GameObject Credits;
+    public GameObject LevelSelect;
+    public GameObject MenuMain;
 
 
+    private void Start()
+    {
+        MenuMain = GameObject.Find("MainMenu");
+        Credits = GameObject.Find("CreditsMenu");
+        LevelSelect = GameObject.Find("LevelMenu");
 
+        MenuMain.SetActive(true);
+        Credits.SetActive(false);
+        LevelSelect.SetActive(false);
+    }
 
     public void Button_start()
     {
@@ -26,7 +34,9 @@ public class mainMenu : MonoBehaviour
 
     public void Button_selectLevel()
     {
-        SceneManager.LoadScene(LevelSelect);
+        //SceneManager.LoadScene(LevelSelect);
+        LevelSelect.SetActive(true);
+        MenuMain.SetActive(false);
     }
     public void Button_Quit()
     {
@@ -34,6 +44,15 @@ public class mainMenu : MonoBehaviour
     }
     public void Button_Credits()
     {
-       SceneManager.LoadScene(Credits);
+        //SceneManager.LoadScene(Credits);
+        Credits.SetActive(true);
+        MenuMain.SetActive(false);
+    }
+
+    public void Button_Back()
+    {
+        MenuMain.SetActive(true);
+        Credits.SetActive(false);
+        LevelSelect.SetActive(false);
     }
 }
